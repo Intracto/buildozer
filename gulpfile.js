fee34d28fc6529e42ea196ef3047fa5ff5249f0c
Ruben Jacobs <ruben6jacobs@gmail.com>
2019-11-08 14:26:21 +0100

    Remove dest_base_path in watch task (#64)
    Fixes a bug in the watch task that added the destination base path to the name of the file

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ function watchFiles() {
   config['js-concat'].forEach(js => {
     const src = config.src_base_path + js.src;
     const dest = config.dest_base_path + js.dest;
-    const name = config.dest_base_path + js.name;
+    const {name} = js;
 
     // eslint-disable-next-line func-names
     gulp.watch(src, function concat() {
@@ -68,7 +68,7 @@ function watchFiles() {
   config['svg-sprite'].forEach(js => {
     const src = config.src_base_path + js.src;
     const dest = config.dest_base_path + js.dest;
-    const name = config.dest_base_path + js.name;
+    const {name} = js;
 
     // eslint-disable-next-line func-names
     gulp.watch(src, function svgSpriteCreate() {
