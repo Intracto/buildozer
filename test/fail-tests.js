591d23ccc0bfe4d18bfcceab873fe52189ae46e2
Martijn Cuppens <martijn.cuppens@gmail.com>
2020-02-13 16:21:50 +0100

    Add ESLint support

--- test/fail-tests.js
+++ test/fail-tests.js
@@ -3,10 +3,14 @@
 const {exec} = require('child_process');
 const chalk = require('chalk');
 
-exec('npm run fail-stylelint', error => {
-  if (!error) {
-    throw new Error('`npm run fail-stylelint` should fail but doesn\'t.');
-  }
+const tests = ['npm run fail-stylelint', 'npm run fail-eslint'];
 
-  console.log(`${chalk.green('✓')}  Failtest succeeded.`);
+tests.forEach(test => {
+  exec(test, error => {
+    if (!error) {
+      throw new Error(`\`${test}\` should fail but doesn't.`);
+    }
+
+    console.log(`${chalk.green('✓')}  Failtest \`${test}\` succeeded.`);
+  });
 });
