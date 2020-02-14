aff276138046661a19dfc732424af8049e41f784
Martijn Cuppens <martijn.cuppens@gmail.com>
2020-02-14 14:41:41 +0100

    Add update notifier

--- bin/buildozer.js
+++ bin/buildozer.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
+const updateNotifier = require('update-notifier');
 const argsInclude = require('../lib/args-include');
+const pkg = require('../package.json');
+
+updateNotifier({pkg}).notify();
 
 if (argsInclude(['build', 'watch', 'css', 'img', 'js', 'clean', 'copy', 'js-concat', 'svg-sprite'])) {
   // Run gulp
